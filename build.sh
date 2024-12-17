8c6613bf63f831e7113ba51bb5ebaade35beee7a
Leonardo Renner <leonardorenner96@gmail.com>
2024-12-16 21:13:28 -0300

    Atualizando requirements.txt e configurações do whitenoise

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 # exit on error
 set -o errexit
 
+# Criar pasta static se não existir
+mkdir -p static
+
+# Atualizar pip
+python -m pip install --upgrade pip
+
+# Instalar dependências
 pip install -r requirements.txt
 
+# Coletar arquivos estáticos e migrar banco de dados
 python manage.py collectstatic --no-input
 python manage.py migrate
