4fe46232d56f9be7b27952873c2ae7cce0cad6a5
Leonardo Renner <leonardorenner96@gmail.com>
2024-12-20 11:57:50 -0300

    Add migrations for CPF and email fields

--- build.sh
+++ build.sh
@@ -4,5 +4,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Limpar migrações existentes (opcional, use com cuidado)
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+
+python manage.py makemigrations
+python manage.py migrate --noinput
 python manage.py collectstatic --no-input
-python manage.py migrate
